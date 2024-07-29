4803d11d13bdedf2dfafd911739b41d4638e3483
Samuel <samuelgranvik@protonmail.com>
2024-07-29 20:13:14 +0300

    1.6 done.
    - Simple state management, clicking on buttons working, gone on page reload.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // State management
+  const goodClicks = () => setGood(good + 1);
+  const neutralClick = () => setNeutral(neutral + 1);
+  const badClicks = () => setBad(bad + 1);
+
   return (
     <div>
       <h1>Unicafe</h1>
       <h2>Give feedback</h2>
 
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
+      <button onClick={goodClicks}>Good</button>
+      <button onClick={neutralClick}>Neutral</button>
+      <button onClick={badClicks}>Bad</button>
+
 
       <h2>Statistics</h2>
       <ul>
-        <li>Good: </li>
-        <li>Neutral: </li>
-        <li>Bad: </li>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
         <li>Average: </li>
         <li>Positive: </li>
       </ul>
