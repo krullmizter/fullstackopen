be4c49a10c86afcc0e5bd423cba63be48c8efb4c
Samuel <samuelgranvik@protonmail.com>
2024-07-29 20:22:37 +0300

    1.7 done
    - Added statistics based on the feedback
    - Cleaned up the code a bit, added some comments

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,23 +10,29 @@ const App = () => {
   const neutralClick = () => setNeutral(neutral + 1);
   const badClicks = () => setBad(bad + 1);
 
+  // Statistic variables
+  const total = good + neutral + bad;
+  const avgFeedback = total > 0 ? (good - bad) / total : 0;
+  const posFeedback = total > 0 ? (good / total) * 100 : 0;
+
   return (
     <div>
       <h1>Unicafe</h1>
       <h2>Give feedback</h2>
 
-      <button onClick={goodClicks}>Good</button>
-      <button onClick={neutralClick}>Neutral</button>
-      <button onClick={badClicks}>Bad</button>
-
+      <div>
+        <button onClick={goodClicks}>Good</button>
+        <button onClick={neutralClick}>Neutral</button>
+        <button onClick={badClicks}>Bad</button>
+      </div>
 
       <h2>Statistics</h2>
       <ul>
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
         <li>Bad: {bad}</li>
-        <li>Average: </li>
-        <li>Positive: </li>
+        <li>Average: {avgFeedback.toFixed(2)}</li>
+        <li>Positive: {posFeedback.toFixed(0)}%</li>
       </ul>
     </div>
   )
