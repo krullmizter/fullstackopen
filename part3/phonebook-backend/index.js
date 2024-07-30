458d124e664dfd912e3603436c642d475b07c0cb
Samuel <samuelgranvik@protonmail.com>
2024-07-30 19:43:29 +0300

    Forgot to commit these

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,81 +1,74 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const dbPath = path.join(__dirname, "db.json");
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
 
-morgan.token("body", (req) => {
-  return JSON.stringify(req.body);
-});
-
+morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const readDb = () => {
+  const data = fs.readFileSync(dbPath);
+  return JSON.parse(data);
+};
+
+const writeDb = (data) => {
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+};
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  const db = readDb();
+  res.json(db.persons);
 });
 
 app.get("/info", (req, res) => {
-  const amountPeople = persons.length;
-  const date = new Date();
+  const db = readDb();
   res.send(
-    `<p>Phonebook has info for ${amountPeople} people</p><p>${date}</p>`
+    `<p>Phonebook has info for ${
+      db.persons.length
+    } people</p><p>${new Date()}</p>`
   );
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
+  const db = readDb();
+  const person = db.persons.find((p) => p.id === req.params.id);
   if (person) {
     res.json(person);
   } else {
-    res.status(404).send({ error: "Person not found" });
-    console.error(res);
+    res.status(404).json({ error: "Person not found" });
   }
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
+  const db = readDb();
+  const personIndex = db.persons.findIndex((p) => p.id === req.params.id);
+  if (personIndex === -1) {
+    return res.status(404).json({ error: "Person not found" });
+  }
+
+  db.persons.splice(personIndex, 1);
+  writeDb(db);
   res.status(204).end();
 });
 
 app.post("/api/persons", (req, res) => {
   const { name, number } = req.body;
-
   if (!name || !number) {
-    return res.status(400).json({ error: "Either name or number is missing!" });
+    return res.status(400).json({ error: "Name or number is missing!" });
   }
 
-  if (persons.some((person) => person.name === name)) {
-    return res.status(400).json({ error: "The name must be unique!" });
+  const db = readDb();
+  if (db.persons.some((p) => p.name === name)) {
+    return res.status(400).json({ error: "Name must be unique!" });
   }
 
   const newPerson = {
@@ -83,12 +76,29 @@ app.post("/api/persons", (req, res) => {
     name,
     number,
   };
-
-  persons = persons.concat(newPerson);
+  db.persons.push(newPerson);
+  writeDb(db);
   res.json(newPerson);
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on localhost:${PORT}`);
+app.put("/api/persons/:id", (req, res) => {
+  const { name, number } = req.body;
+  if (!name || !number) {
+    return res.status(400).json({ error: "Name or number is missing!" });
+  }
+
+  const db = readDb();
+  const personIndex = db.persons.findIndex((p) => p.id === req.params.id);
+  if (personIndex === -1) {
+    return res.status(404).json({ error: "Person not found!" });
+  }
+
+  db.persons[personIndex] = { ...db.persons[personIndex], name, number };
+  writeDb(db);
+  res.json(db.persons[personIndex]);
 });
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
