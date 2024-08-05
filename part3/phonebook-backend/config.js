c5d3c8e8899d714822d9ba7dd9d60a727ba50994
Samuel <samuelgranvik@protonmail.com>
2024-08-05 21:58:29 +0300

    3.22 done, and entire part3 completed
    - Ran eslint och both front and backend, and fixed the lint issues before this commit
    - Began the structure of part4

--- part3/phonebook-backend/config.js
+++ part3/phonebook-backend/config.js
@@ -1,8 +1,8 @@
-const ENV = "production";
+const ENV = 'production'
 
 const config = {
   baseUrl:
-    ENV === "production"
+    ENV === 'production'
       ? process.env.PROD_BACKEND_URL
       : process.env.DEV_BACKEND_URL,
   constants: {
@@ -12,23 +12,23 @@ const config = {
     NOTIFICATION_TIMEOUT: 5000,
   },
   errorMessages: {
-    invalidName: "Invalid name",
-    invalidNumber: "Invalid number",
-    nonJsonResponse: "Received non-JSON response",
+    invalidName: 'Invalid name',
+    invalidNumber: 'Invalid number',
+    nonJsonResponse: 'Received non-JSON response',
     invalidPhone:
-      "is already added to phonebook, replace the old number with a new one?",
+      'is already added to phonebook, replace the old number with a new one?',
     numberLength:
-      "Phone number must be in the format: XX-XXXXXXX or XXX-XXXXXXX and at least 8 characters long",
-    nameLength: "Name must be at least three characters long",
-    tooManyRequests: "Too many requests, please try again later",
-    invalidId: "Invalid ID format",
-    personNotFound: "Person not found",
+      'Phone number must be in the format: XX-XXXXXXX or XXX-XXXXXXX and at least 8 characters long',
+    nameLength: 'Name must be at least three characters long',
+    tooManyRequests: 'Too many requests, please try again later',
+    invalidId: 'Invalid ID format',
+    personNotFound: 'Person not found',
   },
   limit: {
     windowMs: 15 * 60 * 1000,
     max: 100,
-    message: "Too many requests, please try again later",
+    message: 'Too many requests, please try again later',
   },
-};
+}
 
-module.exports = config;
+module.exports = config
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,189 +1,189 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const Person = require("./models/personModel");
-const { errorHandler, commonMiddleware, logger } = require("./middleware");
-const cors = require("cors");
-const { body, validationResult, param } = require("express-validator");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const config = require("./config");
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Person = require('./models/personModel')
+const { errorHandler, commonMiddleware, logger } = require('./middleware')
+const cors = require('cors')
+const { body, validationResult, param } = require('express-validator')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const config = require('./config')
 
-const { errorMessages, limit } = config;
+const { errorMessages, limit } = config
 
-const app = express();
+const app = express()
 
 // Middleware
 const allowedOrigins = [
   process.env.DEV_FRONTEND_URL,
   process.env.PROD_FRONTEND_URL,
-];
+]
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error("IP/URL not allowed by CORS"));
+      callback(new Error('IP/URL not allowed by CORS'))
     }
   },
   optionsSuccessStatus: 200,
-};
+}
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(helmet());
-commonMiddleware(app);
-logger(app);
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(helmet())
+commonMiddleware(app)
+logger(app)
 
-const limiter = rateLimit(limit);
-app.use(limiter);
+const limiter = rateLimit(limit)
+app.use(limiter)
 
 // MongoDB connection setup
-const mongoUrl = process.env.MONGODB_URL;
-const PORT = process.env.PORT || 3001;
+const mongoUrl = process.env.MONGODB_URL
+const PORT = process.env.PORT || 3001
 
 mongoose
-  .set("strictQuery", false)
+  .set('strictQuery', false)
   .connect(mongoUrl, {})
   .then(() => {
-    console.log("Connected to database");
+    console.log('Connected to database')
 
-    app.listen(PORT);
+    app.listen(PORT)
   })
   .catch((err) => {
-    console.error("Database connection error:", err);
-    process.exit(1);
-  });
+    console.error('Database connection error:', err)
+    process.exit(1)
+  })
 
 // Routes
-app.get("/api/persons", async (req, res, next) => {
+app.get('/api/persons', async (req, res, next) => {
   try {
-    const persons = await Person.find({});
-    res.json(persons);
+    const persons = await Person.find({})
+    res.json(persons)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.get("/info", async (req, res, next) => {
+app.get('/info', async (req, res, next) => {
   try {
-    const persons = await Person.find({});
+    const persons = await Person.find({})
     res.send(
       `<p>Phonebook has info for ${
         persons.length
       } people</p><p>${new Date()}</p>`
-    );
+    )
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 app.get(
-  "/api/persons/:id",
-  [param("id").isMongoId().withMessage(errorMessages.invalidId)],
+  '/api/persons/:id',
+  [param('id').isMongoId().withMessage(errorMessages.invalidId)],
   async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() })
     }
 
     try {
-      const person = await Person.findById(req.params.id);
+      const person = await Person.findById(req.params.id)
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).json({ error: errorMessages.personNotFound });
+        res.status(404).json({ error: errorMessages.personNotFound })
       }
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-);
+)
 
 app.post(
-  "/api/persons",
+  '/api/persons',
   [
-    body("name").isLength({ min: 3 }).trim().escape(),
-    body("number")
+    body('name').isLength({ min: 3 }).trim().escape(),
+    body('number')
       .matches(/^\d{2,3}-\d+$/)
       .isLength({ min: 8 })
       .trim()
       .escape(),
   ],
   async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() })
     }
 
-    const { name, number } = req.body;
+    const { name, number } = req.body
 
     try {
-      const newPerson = new Person({ name, number });
-      const savedPerson = await newPerson.save();
-      res.status(201).json(savedPerson);
+      const newPerson = new Person({ name, number })
+      const savedPerson = await newPerson.save()
+      res.status(201).json(savedPerson)
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-);
+)
 
 app.put(
-  "/api/persons/:id",
+  '/api/persons/:id',
   [
-    param("id").isMongoId().withMessage(errorMessages.invalidId),
-    body("name").isLength({ min: 3 }).trim().escape(),
-    body("number")
+    param('id').isMongoId().withMessage(errorMessages.invalidId),
+    body('name').isLength({ min: 3 }).trim().escape(),
+    body('number')
       .matches(/^\d{2,3}-\d+$/)
       .isLength({ min: 8 })
       .trim()
       .escape(),
   ],
   async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() })
     }
 
-    const { name, number } = req.body;
+    const { name, number } = req.body
 
     try {
       const updatedPerson = await Person.findByIdAndUpdate(
         req.params.id,
         { name, number },
-        { new: true, runValidators: true, context: "query" }
-      );
+        { new: true, runValidators: true, context: 'query' }
+      )
       if (updatedPerson) {
-        res.json(updatedPerson);
+        res.json(updatedPerson)
       } else {
-        res.status(404).json({ error: errorMessages.personNotFound });
+        res.status(404).json({ error: errorMessages.personNotFound })
       }
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-);
+)
 
 app.delete(
-  "/api/persons/:id",
-  [param("id").isMongoId().withMessage(errorMessages.invalidId)],
+  '/api/persons/:id',
+  [param('id').isMongoId().withMessage(errorMessages.invalidId)],
   async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() })
     }
 
     try {
-      const result = await Person.findByIdAndDelete(req.params.id);
+      const result = await Person.findByIdAndDelete(req.params.id)
       if (!result) {
-        return res.status(404).json({ error: errorMessages.personNotFound });
+        return res.status(404).json({ error: errorMessages.personNotFound })
       }
-      res.status(204).end();
+      res.status(204).end()
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-);
+)
 
-app.use(errorHandler);
+app.use(errorHandler)
