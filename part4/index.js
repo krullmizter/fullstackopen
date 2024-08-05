74a41b96b47ff503e29c0300ebb53ac0f5203a09
Samuel <samuelgranvik@protonmail.com>
2024-08-05 22:36:35 +0300

    4.1 done
    - Removed old files
    - Crated a new MongoDB, blogs
    - Able to GET and POST from the db

--- part4/index.js
+++ part4/index.js
@@ -1,39 +1,61 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+app.use(cors());
+app.use(express.json());
+
+// Schema
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number,
+  likes: { type: Number, default: 0 },
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
 
+// MongoDB connection & Mongoose configuration
 const mongoUrl = process.env.MONGODB_URL;
+mongoose
+  .connect(mongoUrl, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+// Routes
+app.get("/api/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.find({});
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  const { title, author, url, likes } = req.body;
+
+  if (!title || !url) {
+    return res.status(400).json({ error: "Title and URL are required" });
+  }
+
+  try {
+    const newBlog = new Blog({ title, author, url, likes });
+    const savedBlog = await newBlog.save();
+    res.status(201).json(savedBlog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Starting server certain port
 const PORT = process.env.PORT || 3001;
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
