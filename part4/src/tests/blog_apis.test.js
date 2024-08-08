5c703524f754a07ddd4aab9f1af5354e866aca51
Samuel <samuelgranvik@protonmail.com>
2024-08-08 14:38:57 +0300

    4.11-4.12* done

--- part4/src/tests/blog_apis.test.js
+++ part4/src/tests/blog_apis.test.js
@@ -9,7 +9,7 @@ const Blog = require("../models/blog");
 const api = supertest(app);
 
 before(async () => {
-  console.log("Running tests for Blog APIs");
+  console.log("Running tests for blog APIs");
   console.log(`Using environment: ${config.env}\n`);
 });
 
@@ -31,16 +31,12 @@ test("Blogs count is correct, and uses JSON", async () => {
     likes: 0,
   }).save();
 
-  const response = await api
+  const res = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
-  assert.strictEqual(
-    response.body.length,
-    1,
-    "The amount of blogs returned is incorrect"
-  );
+  assert.strictEqual(res.body.length, 1, "The number of blogs are incorrect");
 });
 
 // 4.9
@@ -53,28 +49,26 @@ test("Unique _id is named id", async () => {
   });
   await newBlog.save();
 
-  const response = await api
+  const res = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
-  const blogFromResponse = response.body.find(
-    (blog) => blog.title === "Blog 4.9"
-  );
+  const blogFromRes = res.body.find((blog) => blog.title === "Blog 4.9");
 
-  assert.ok(blogFromResponse, "Blog was not found in the response");
-  assert.ok(blogFromResponse.id, "The blog does not have an 'id' field");
+  assert.ok(blogFromRes, "Blog was not found in the response");
+  assert.ok(blogFromRes.id, "The blog does not have an 'id' field");
   assert.strictEqual(
-    blogFromResponse.id,
+    blogFromRes.id,
     newBlog.id.toString(),
     "Blog 'id' does not match"
   );
 });
 
 // 4.10
-test("Successfully using POST to create a new blog", async () => {
-  const initialBlogs = await Blog.find({});
-  const initialCount = initialBlogs.length;
+test("Successfully crated a new blog with POST", async () => {
+  const blogsBefore = await Blog.find({});
+  const blogsCountBefore = blogsBefore.length;
 
   const newBlog = {
     title: "Blog 4.10",
@@ -92,12 +86,56 @@ test("Successfully using POST to create a new blog", async () => {
   const blogsAfterPost = await Blog.find({});
   assert.strictEqual(
     blogsAfterPost.length,
-    initialCount + 1,
-    "Blog count did not increase"
+    blogsCountBefore + 1,
+    "Blog count did NOT increase"
   );
 
   const titles = blogsAfterPost.map((blog) => blog.title);
-  assert.ok(titles.includes(newBlog.title), "No new blog title was found");
+  assert.ok(titles.includes(newBlog.title), "No new title was found");
+});
+
+// 4.11
+test("If the likes value is missing, it should be set to 0", async () => {
+  const newBlog = {
+    title: "Blog 4.11",
+    author: "Jane Doe",
+    url: "https://google.com",
+  };
+
+  const res = await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  console.log(res.body);
+
+  assert.strictEqual(
+    res.body.likes,
+    0,
+    "The value of 'likes' was NOT set to 0"
+  );
+});
+
+// 4.12
+test("400 status if blog title is missing from the response", async () => {
+  const newBlog = {
+    author: "John Doe",
+    url: "https://google.com",
+    likes: 15,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
+});
+
+test("400 status if blog URL is missing from the response", async () => {
+  const newBlog = {
+    title: "Blog 4.12",
+    author: "Jane Doe",
+    likes: 25,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
 });
 
 // CLose the DB connection after all tests
